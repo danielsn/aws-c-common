6d07bcb140ef4a564d650722ec6460803a3e4ca4
Felipe R. Monteiro <felisous@amazon.com>
2019-03-05 10:33:37 -0500

    Update harnesses for array list proofs
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/include/proof_helpers/make_common_data_structures.h
+++ .cbmc-batch/include/proof_helpers/make_common_data_structures.h
@@ -17,20 +17,31 @@
 
 #include <aws/common/array_list.h>
 #include <aws/common/common.h>
+#include <proof_helpers/proof_allocators.h>
 
-#include "nondet.h"
+#include <proof_helpers/nondet.h>
 
 #include <stdlib.h>
 
-#define MAX_INITIAL_ITEM_ALLOCATION 2
+#define MAX_INITIAL_ITEM_ALLOCATION 5
 #define MAX_ITEM_SIZE 2
-#define MAX_STR_LEN 32
-#define MAX_BUF_LEN 32
+#define MAX_STR_LEN 2
+#define MAX_BUF_LEN 2
 
 #define ASSUME_VALID_MEMORY(ptr) ptr = malloc(sizeof(*(ptr)))
 #define ASSUME_VALID_MEMORY_COUNT(ptr, count) ptr = malloc(sizeof(*(ptr)) * (count))
 #define ASSUME_DEFAULT_ALLOCATOR(allocator) allocator = aws_default_allocator()
-#define ASSUME_ARBITRARY_ARRAY_LIST(list, item_count, item_size)                                                       \
-    list = make_arbitrary_array_list((item_count), (item_size))
+#define ASSUME_CAN_FAIL_ALLOCATOR(allocator) allocator = can_fail_allocator()
+#define ASSUME_ARBITRARY_ARRAY_LIST(list, initial_item_allocation, item_size)                                          \
+    list = make_arbitrary_array_list((initial_item_allocation), (item_size))
+#define ASSUME_NONDET_ARRAY_LIST(list) list = make_nondet_array_list()
+#define ASSUME_BOUNDED_ARRAY_LIST(list, max_initial_item_allocation, max_item_size)                                    \
+    list = make_bounded_array_list((max_initial_item_allocation), (max_item_size))
 
-struct aws_array_list *make_arbitrary_array_list(size_t item_count, size_t item_size);
+struct aws_array_list *make_arbitrary_array_list(size_t initial_item_allocation, size_t item_size);
+
+struct aws_array_list *make_nondet_array_list();
+
+struct aws_array_list *make_bounded_array_list(size_t max_initial_item_allocation, size_t max_item_size);
+
+int compare(const void *a, const void *b);
--- .cbmc-batch/source/make_common_data_structures.c
+++ .cbmc-batch/source/make_common_data_structures.c
@@ -13,31 +13,70 @@
  * limitations under the License.
  */
 
-#include "proof_helpers/make_common_data_structures.h"
+#include <proof_helpers/make_common_data_structures.h>
 
-/**
- * It nondeterministically creates either a static or dynamic array list
- * structure, which is bounded by item_count and item_size parameters.
- */
-struct aws_array_list *make_arbitrary_array_list(size_t item_count, size_t item_size) {
+struct aws_array_list *make_arbitrary_array_list(size_t initial_item_allocation, size_t item_size) {
+    __CPROVER_assume(item_size != 0);
     struct aws_array_list *list;
+
     /* Assume list is allocated */
     ASSUME_VALID_MEMORY(list);
 
-    if (nondet_int()) { /* Dynamic initialization */
-        /* Use default allocator */
+    if (nondet_int()) { /* dynamic */
         struct aws_allocator *allocator = malloc(sizeof(*allocator));
-        ASSUME_DEFAULT_ALLOCATOR(allocator);
-        __CPROVER_assume(!aws_array_list_init_dynamic(list, allocator, item_count, item_size));
-    } else { /* Static initialization */
-        __CPROVER_assume(item_count > 0);
-        __CPROVER_assume(item_size > 0);
-
-        size_t len = item_count * item_size;
-        void *raw_array = malloc(len);
-
-        aws_array_list_init_static(list, raw_array, item_count, item_size);
+        ASSUME_CAN_FAIL_ALLOCATOR(allocator);
+        list->alloc = allocator;
+        size_t allocation_size;
+        __CPROVER_assume(!aws_mul_size_checked(initial_item_allocation, item_size, &allocation_size));
+        list->data = NULL;
+        list->item_size = item_size;
+        list->current_size = 0;
+        list->length = 0;
+        if (allocation_size > 0) {
+            list->data = aws_mem_acquire(list->alloc, allocation_size);
+            __CPROVER_assume(list->data != NULL);
+            list->current_size = allocation_size;
+        }
+        __CPROVER_assume(list->current_size == 0 || list->data);
+    } else { /* static */
+        size_t len;
+        __CPROVER_assume(!aws_mul_size_checked(initial_item_allocation, item_size, &len));
+        void *raw_array = can_fail_malloc(len);
+        __CPROVER_assume(raw_array != NULL);
+        __CPROVER_assume(initial_item_allocation != 0);
+        list->alloc = NULL;
+        __CPROVER_assume(!aws_mul_size_checked(initial_item_allocation, item_size, &list->current_size));
+        list->item_size = item_size;
+        list->length = 0;
+        list->data = raw_array;
     }
-
+    list->length = nondet_size_t();
+    __CPROVER_assume(list->length <= initial_item_allocation);
     return list;
+}
+
+struct aws_array_list *make_nondet_array_list() {
+    size_t initial_item_allocation = nondet_size_t();
+    size_t item_size = nondet_size_t();
+    struct aws_array_list *list;
+    ASSUME_ARBITRARY_ARRAY_LIST(list, initial_item_allocation, item_size);
+    return list;
+}
+
+struct aws_array_list *make_bounded_array_list(size_t max_initial_item_allocation, size_t max_item_size) {
+    size_t initial_item_allocation = nondet_size_t();
+    __CPROVER_assume(initial_item_allocation <= max_initial_item_allocation);
+    size_t item_size = nondet_size_t();
+    __CPROVER_assume(item_size <= max_item_size);
+    struct aws_array_list *list;
+    ASSUME_ARBITRARY_ARRAY_LIST(list, initial_item_allocation, item_size);
+    return list;
+}
+
+int compare(const void *a, const void *b) {
+    size_t n = nondet_size_t();
+    __CPROVER_assume(n <= MAX_ITEM_SIZE);
+    __CPROVER_precondition(__CPROVER_r_ok(a, n), "first element readable in compare function");
+    __CPROVER_precondition(__CPROVER_r_ok(b, n), "second element readable in compare function");
+    return nondet_int();
 }
