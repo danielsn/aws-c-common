44df5af9dad1e6b45ce31eef49726cc8bf6bba71
Felipe R. Monteiro <felisous@amazon.com>
2019-03-05 10:33:37 -0500

    Update harnesses for array list proofs
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/jobs_in_progress/aws_array_list_push_back/aws_array_list_push_back_harness.c
+++ .cbmc-batch/jobs_in_progress/aws_array_list_push_back/aws_array_list_push_back_harness.c
@@ -1,15 +1,48 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 #include <aws/common/array_list.h>
 #include <proof_helpers/make_common_data_structures.h>
 
+#define MAX_INITIAL_ITEM_ALLOCATION 5
+#define MAX_ITEM_SIZE 2
+
+/**
+ * Coverage:
+ * Runtime:
+ *
+ * Assumptions:
+ *     -
+ */
 void aws_array_list_push_back_harness() {
-    size_t item_count = nondet_size_t();
-    __CPROVER_assume(item_count <= MAX_INITIAL_ITEM_ALLOCATION);
-    size_t item_size = nondet_size_t();
-    __CPROVER_assume(item_size <= MAX_ITEM_SIZE);
     struct aws_array_list *list;
-    ASSUME_ARBITRARY_ARRAY_LIST(list, item_count, item_size);
+    ASSUME_BOUNDED_ARRAY_LIST(list, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE);
+    struct aws_allocator *alloc = list->alloc;
+    size_t length = list->length;
+    size_t item_size = list->item_size;
 
-    void *val = malloc(item_size);
+    void *val = malloc(list->item_size);
 
-    aws_array_list_push_back(list, val);
+    if (!aws_array_list_push_back(list, val)) {
+        /* some guarantees */
+        assert(list->length == length + 1);
+    } else {
+        /* some guarantees */
+        assert(list->length == length);
+    }
+    /* some guarantees */
+    assert(list->alloc == alloc);
+    assert(list->item_size == item_size);
 }
--- .cbmc-batch/jobs_in_progress/aws_array_list_set_at/aws_array_list_set_at_harness.c
+++ .cbmc-batch/jobs_in_progress/aws_array_list_set_at/aws_array_list_set_at_harness.c
@@ -1,18 +1,51 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 #include <aws/common/array_list.h>
 #include <proof_helpers/make_common_data_structures.h>
 
-void aws_array_list_set_at_harness() {
-    size_t initial_item_allocation = nondet_size_t();
-    __CPROVER_assume(initial_item_allocation <= MAX_INITIAL_ITEM_ALLOCATION);
-    size_t item_size = nondet_size_t();
-    __CPROVER_assume(item_size <= MAX_ITEM_SIZE);
-    struct aws_array_list *list;
-    ASSUME_ARBITRARY_ARRAY_LIST(list, initial_item_allocation, item_size);
+#define MAX_INITIAL_ITEM_ALLOCATION 5
+#define MAX_ITEM_SIZE 2
 
-    void *val = malloc(item_size);
+/**
+ * Coverage:
+ * Runtime:
+ *
+ * Assumptions:
+ *     -
+ */
+void aws_array_list_set_at_harness() {
+    struct aws_array_list *list;
+    ASSUME_BOUNDED_ARRAY_LIST(list, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE);
+    struct aws_allocator *alloc = list->alloc;
+    size_t length = list->length;
+    size_t item_size = list->item_size;
+
+    void *val = malloc(list->item_size);
 
     size_t index = nondet_size_t();
-    __CPROVER_assume(index < __CPROVER_constant_infinity_uint - 1);
 
-    aws_array_list_set_at(list, val, index);
+    if (!aws_array_list_set_at(list, val, index)) {
+        /* some guarantees */
+        if (index > length)
+            assert(list->length == index + 1);
+    } else {
+        /* some guarantees */
+        assert(list->length == length);
+    }
+    /* some guarantees */
+    assert(list->alloc == alloc);
+    assert(list->item_size == item_size);
 }
